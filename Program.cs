1c29b2df5b09967609e7a683affe106455ed3775
thanhdnh <myhoangthanh@yahoo.com>
2023-01-31 19:21:52 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@
     Console.WriteLine("{0}/{1}={2}", a, b, thuong);*/
 
     //Bài 2. |a-b|, a, b là hai số thực cho trước
-    float a;
+    /*float a;
     float b;
     Console.WriteLine("Nhập hai số a, b: ");
     string s_a = Console.ReadLine();//"123"=>123
@@ -34,7 +34,49 @@
 
     float gttd;
     gttd = (a>=b)?(a-b):(b-a);
-    System.Console.WriteLine($"|{a}-{b}|={gttd}");
+    System.Console.WriteLine($"|{a}-{b}|={gttd}");*/
+
+    //Bài 3. Tìm số lớn nhất trong 2 số a, b
+    /*float a;
+    float b;
+    Console.WriteLine("Nhập hai số a, b: ");
+    string s_a = Console.ReadLine();//"123"=>123
+    string s_b = Console.ReadLine();
+    a = float.Parse(s_a);
+    b = float.Parse(s_b);
+    float max;
+    max = (a>b)?a:b;
+    System.Console.WriteLine($"max({a}, {b})={max}");*/
+
+    //Bài 4. Tìm số lớn nhất trong 3 số a, b, c
+    /*float a, b, c;
+    Console.WriteLine("Nhập hai số a, b, c: ");
+    string s_a = Console.ReadLine();//"123"=>123
+    string s_b = Console.ReadLine();
+    string s_c = Console.ReadLine();
+    a = float.Parse(s_a);
+    b = float.Parse(s_b);
+    c = float.Parse(s_c);
+    float max = (a>=b)?a:b;
+    max = (max>=c)?max:c;
+    //max = (a>=b)?((a>c)?a:c):((b>=c)?b:c);
+    System.Console.WriteLine($"max({a}, {b}, {c})={max}");*/
+
+    //Bài 5. Tìm số lớn nhất trong 4 số a, b, c, d
+    float a, b, c, d;
+    Console.WriteLine("Nhập hai số a, b, c, d: ");
+    string s_a = Console.ReadLine();//"123"=>123
+    string s_b = Console.ReadLine();
+    string s_c = Console.ReadLine();
+    string s_d = Console.ReadLine();
+    a = float.Parse(s_a);
+    b = float.Parse(s_b);
+    c = float.Parse(s_c);
+    d = float.Parse(s_d);
+    float max1 = (a>=b)?a:b;
+    float max2 = (c>=d)?c:d;
+    float max = (max1>=max2)?max1:max2;
+    System.Console.WriteLine($"max({a}, {b}, {c}, {d})={max}");
 
     Console.ReadKey();
   }
