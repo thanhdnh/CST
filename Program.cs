b2c7a7d7ea6dd0aa9f5e77507dcb17776edbb957
thanhdnh <myhoangthanh@yahoo.com>
2023-01-31 18:27:36 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Clear();
+
+    //Nhập vào hai số a, b => Tính tổng, hiệu, tích và thương của a và b
+
+    Console.ReadKey();
+  }
+}
